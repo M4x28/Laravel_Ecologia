59d7302821d749874815db8e8740381e7fe98b60
Leonardo <leobir68@gmail.com>
2022-03-23 12:19:33 +0100

    ok6

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,13 +15,38 @@
                     <div class="card-header">{{ __('Dashboard') }}</div>
 
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+                        <!-- Pills navs -->
+                        <ul class="nav nav-pills nav-justified mb-3" id="ex1" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link active" id="ex3-tab-1" data-mdb-toggle="pill" href="#ex3-pills-1"
+                                    role="tab" aria-controls="ex3-pills-1" aria-selected="true">{{ __('Add City') }}</a>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link" id="ex3-tab-2" data-mdb-toggle="pill" href="#ex3-pills-2"
+                                    role="tab" aria-controls="ex3-pills-2" aria-selected="false">{{ __('Add CCR') }}</a>
+                            </li>
+                            <li class="nav-item" role="presentation">
+                                <a class="nav-link" id="ex3-tab-3" data-mdb-toggle="pill" href="#ex3-pills-3"
+                                    role="tab" aria-controls="ex3-pills-3"
+                                    aria-selected="false">{{ __('Add Calendar') }}</a>
+                            </li>
+                        </ul>
+                        <!-- Pills navs -->
 
-                        {{ __('You are logged in!') }}
+                        <!-- Pills content -->
+                        <div class="tab-content" id="ex2-content">
+                            <div class="tab-pane fade show active" id="ex3-pills-1" role="tabpanel"
+                                aria-labelledby="ex3-tab-1">
+                                Tab 1 content
+                            </div>
+                            <div class="tab-pane fade" id="ex3-pills-2" role="tabpanel" aria-labelledby="ex3-tab-2">
+                                Tab 2 content
+                            </div>
+                            <div class="tab-pane fade" id="ex3-pills-3" role="tabpanel" aria-labelledby="ex3-tab-3">
+                                Tab 3 content
+                            </div>
+                        </div>
+                        <!-- Pills content -->
                     </div>
                 </div>
             </div>
