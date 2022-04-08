6a366830a01d795d1dee3b7d7888b24bde4277f6
Leonardo <leobir68@gmail.com>
2022-04-08 12:56:52 +0200

    .

--- resources/views/paese.blade.php
+++ resources/views/paese.blade.php
@@ -15,14 +15,14 @@
                         height="115">
                 </div>
                 <div class="col-lg-8">
-                    <div class="row text-black h5">{{ __('COMUNE DI') }}</div>
+                    <div class="row text-black h5">{{ __('MUNICIPALITY OF') }}</div>
                     <div class="row display-2 text-success">{{ $paese }}</div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="display-5 text-center text-primary m-5">{{ __('Benvenuto nel comune di') }} {{ $paese }}</div>
+    <div class="display-5 text-center text-primary m-5">{{ __('Welcome in') }} {{ $paese }}</div>
 
     <div class="container card d-flex justify-content-center bg-light bg-gradient shadow-custom">
         <div class="row">
@@ -31,12 +31,12 @@
                     alt="mappa" height="300">
             </div>
             <div class="col-md-6 text-center mt-5 mb-4">
-                <h2 class="text-info">{{ __('Informazioni sul Comune') }}</h2>
-                <h4 class="text-black mt-4">{{ __('Regione:') }} {{ $info_comune[0]->regione }}</h4>
-                <h4 class="text-black">{{ __('Provincia:') }} {{ $info_comune[0]->provincia }}</h4>
-                <h4 class="text-black">{{ __('Superficie:') }} {{ $info_comune[0]->superficie }}</h4>
-                <h4 class="text-black">{{ __('Residenti:') }} {{ $info_comune[0]->num_residenti }}</h4>
-                <h5 class="text-black">{{ __('Indirizzo Municipio:') }} {{ $info_comune[0]->indirizzo }}</h5>
+                <h2 class="text-info">{{ __('Info') }}</h2>
+                <h4 class="text-black mt-4">{{ __('Region:') }} {{ $info_comune[0]->regione }}</h4>
+                <h4 class="text-black">{{ __('Province:') }} {{ $info_comune[0]->provincia }}</h4>
+                <h4 class="text-black">{{ __('Sourface:') }} {{ $info_comune[0]->superficie }}</h4>
+                <h4 class="text-black">{{ __('Residents:') }} {{ $info_comune[0]->num_residenti }}</h4>
+                <h5 class="text-black">{{ __('Town Hall Address:') }} {{ $info_comune[0]->indirizzo }}</h5>
             </div>
         </div>
     </div>
@@ -47,31 +47,31 @@
                 <div class='m-5' id='map' style='width: 500px; height: 350px; border-radius:15px;'></div>
             </div>
             <div class="col-md-6 text-center mt-5">
-                <h2 class="text-warning">{{ __('Informazioni sul CCR') }}</h2>
-                <h4 class="text-black mt-4">{{ __('Nome:') }} {{ $ccr[0]->nome }}</h4>
+                <h2 class="text-warning">{{ __('Information on the JRC') }}</h2>
+                <h4 class="text-black mt-4">{{ __('Name:') }} {{ $ccr[0]->nome }}</h4>
                 <h4 class="text-black mt-4">Email: {{ $ccr[0]->email }}</h4>
-                <h4 class="text-black mt-4">{{ __('Indirizzo:') }} {{ $ccr[0]->indirizzo }}</h4>
+                <h4 class="text-black mt-4">{{ __('Address:') }} {{ $ccr[0]->indirizzo }}</h4>
             </div>
         </div>
         <hr>
         <div class="row text-center mt-2 mb-4">
-            <h2 class="text-info">{{ __('Orari di apertura') }}</h2>
-            <h5 class="text-black m-3">{{ __('Lunedì-Venerdì:') }}
+            <h2 class="text-info">{{ __('Opening time') }}</h2>
+            <h5 class="text-black m-3">{{ __('Monday-Friday:') }}
                 {{ $ccr[0]->AperturaSett }}-{{ $ccr[0]->ChiusuraSett }}
             </h5>
             @if ($ccr[0]->AperturaSab != null)
-                <h5 class="text-black m-3">{{ __('Sabato:') }}
+                <h5 class="text-black m-3">{{ __('Saturday:') }}
                     {{ $ccr[0]->AperturaSab }}-{{ $ccr[0]->ChiusuraSab }}</h5>
             @endif
             @if ($ccr[0]->AperturaDom != null)
-                <h5 class="text-black m-3">{{ __('Domenica:') }}
+                <h5 class="text-black m-3">{{ __('Sunday:') }}
                     {{ $ccr[0]->AperturaDom }}-{{ $ccr[0]->ChiusuraDom }}</h5>
             @endif
         </div>
     </div>
 
     <div class="container card d-flex justify-content-center bg-light bg-gradient shadow-custom mt-5">
-        <h2 class="text-secondary text-center mt-4">{{ __('Informazioni sulle Zone') }}</h2>
+        <h2 class="text-secondary text-center mt-4">{{ __('Information on the Areas') }}</h2>
 
         @foreach ($zone as $zona)
             <div class="row">
@@ -80,7 +80,7 @@
                         height="350" width="520">
                 </div>
                 <div class="col-md-6 text-center mt-4">
-                    <h4 class="text-primary">{{ __('Zona:') }} {{ $zone[0]->nome }}</h4>
+                    <h4 class="text-primary">{{ __('Area:') }} {{ $zone[0]->nome }}</h4>
                     <img class='rounded m-4 mt-2'
                         src="{{ asset('../storage/app/public/img_calendari/' . $zona->calendario) }}" alt="calendario"
                         height="310" width="500">
@@ -91,7 +91,7 @@
                     href="{{ asset('../storage/app/public/img_calendari/' . $zona->calendario) }}" role="button"
                     download="{{ $zona->calendario }}">
                     <i class="fas fa-download fa-lg"></i>
-                    <strong>{{ __('Scarica Calendario') }}</strong>
+                    <strong>{{ __('Download Calendar') }}</strong>
                 </a>
             </div>
             <hr>
